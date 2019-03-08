5b0f0ffde0e3b3063315e65a84c4ac16ba2f1fc8
ashipinyin <2238927941@qq.com>
2019-03-08 18:08:05 +0800

    添加数据记录，修复bug

--- exp_v2.py
+++ exp_v2.py
@@ -11,9 +11,9 @@ import numpy as np
 # 初始数据
 tial_times = 30
 
-# 建立存储字典result
+# 建立存储字典result m1不表征m2就近表征m3表征总体
 result = {'name': 'null', 'sex': 'null', 'age': 0, 'average_r': [], 'cover': [], 'estimate': [], 'group': [],
-          'block': [], 'id': [], 'm_1': [], 'm_2': []}
+          'block': [], 'id': [], 'm_1': [], 'm_2': [], 'm_3': [], 'adjust_size': []}
 # gui
 myDlg = gui.Dlg(title="实验")
 myDlg.addText('被试信息')
@@ -43,19 +43,19 @@ sample_size = int(len(dots)/11)
 stims = [0]*4*sample_size
 pos = ['L', 'O', 'R', 'M']*80
 ad = [0]*4
-for i in range(len(ad)):
-    ad[i] = [40, 70]*40
+for i in range(4):
+    ad[i] = [40, 50, 60, 70]*15
     random.shuffle(ad[i])
 adjust_size = ad[0]+ad[1]+ad[2]+ad[3]
 
 for i in range(sample_size):
-    stims[i] = [dots[11*i:11*(i+1)], 0, adjust_size[i]]
+    stims[i] = [dots[11*i:11*(i+1)], 0, 0]
     stims[i][0].index = inde
-    stims[80+i] = [dots[11*i:11*(i+1)], 0, adjust_size[i]]
+    stims[80+i] = [dots[11*i:11*(i+1)], 0, 0]
     stims[80+i][0].index = inde
-    stims[160+i] = [dots[11*i:11*(i+1)], 0, adjust_size[i]]
+    stims[160+i] = [dots[11*i:11*(i+1)], 0, 0]
     stims[160+i][0].index = inde
-    stims[240+i] = [dots[11*i:11*(i+1)], 0, adjust_size[i]]
+    stims[240+i] = [dots[11*i:11*(i+1)], 0, 0]
     stims[240+i][0].index = inde
 # 矩阵赋值，遮位置LORM
 for i in range(sample_size):
@@ -66,9 +66,11 @@ for i in range(sample_size):
 stims = [x for x in stims if x[1] != 'O']
 a = [stims[0:60], stims[60:120], stims[120:180], stims[180:]]
 for each in a:
-    random.shuffle(a)
+    random.shuffle(each)
 random.shuffle(a)
 stims = a[0]+a[1]+a[2]+a[3]
+for i in range(len(stims)):
+    stims[i][2] = adjust_size[i]
 
 # 练习刺激
 tr_stims = [0]*3*3
@@ -81,9 +83,7 @@ for i in range(3):
     tr_stims[3*i+2][0].index = inde
 stims_block = [stims[:60], stims[60:120], stims[120:180], stims[180:]]
 # 随机刺激样本
-random.shuffle(stims)
 random.shuffle(tr_stims)
-random.shuffle(adjust_size)
 # 窗口
 win = visual.Window(size=(w, h), units='pix', fullscr=True, color=(-0.2, -0.2, -0.2))
 myMouse = event.Mouse()
@@ -201,29 +201,39 @@ for i in range(N):
     # 刺激
     model_s1 = np.array([])  # 模型1 不表征
     model_s2 = np.array([])  # 模型2 就进取样
+    model_s3 = np.array([])  # 模型3 表征总体
     aa = []
     for j in range(10):
         x, y, r = stims[i][0]['x'][j], stims[i][0]['y'][j], stims[i][0]['r'][j]
         circle.pos = (x-450, y)
         circle.radius = r
-        model_s2 = np.append(model_s2, r)
         if (x < b_x1) or (x > b_x2):
             circle.draw()
             model_s1 = np.append(model_s1, r)
+            model_s2 = np.append(model_s2, r)
+            model_s3 = np.append(model_s3, r)
         else:
             aa.append(j)
+            if stims[i][0]['group'][10] == 'random':
+                model_s2 = np.append(model_s2, 55)
+                model_s3 = np.append(model_s3, 55)
+            else:
+                model_s2 = np.append(model_s2, r)
+                model_s3 = np.append(model_s3, 0.1*b_x1+25)
     #  模型二元素计算
-    if aa[0] == 0:
-        for each in aa:
-            model_s2[each] = model_s2[aa[-1]]
-    elif aa[-1] == 9:
-        for each in aa:
-            model_s2[each] = model_s2[aa[0]]
-    else:
-        for each in aa:
-            model_s2[each] = (model_s2[aa[-1]]+model_s2[aa[0]])/2
+    if stims[i][0]['group'][10] != 'random':
+        if aa[0] == 0:
+            for each in aa:
+                model_s2[each] = model_s2[aa[-1]]
+        elif aa[-1] == 9:
+            for each in aa:
+                model_s2[each] = model_s2[aa[0]]
+        else:
+            for each in aa:
+                model_s2[each] = (model_s2[aa[-1]]+model_s2[aa[0]])/2
     result['m_1'].append(model_s1.mean())
     result['m_2'].append(model_s2.mean())
+    result['m_3'].append(model_s3.mean())
     # 遮挡
     p_block.pos = (b_x1-300, 0)
     p_block.draw()
@@ -265,12 +275,13 @@ for i in range(N):
 with open("exp_data\\%s.csv" % (result['name']+time.strftime("%H-%M-%S")), 'a') as exp_data:
     exp_data.write(
         'num' + ',' + 'name' + ',' + 'age' + ',' + 'sex' + ',' + 'average_r' + ',' + 'cover' + ','+'estimate_size' + ','
-        + 'group' + ',' + 'model1' + ',' + 'model2' + ',' + 'block' + '\n')
+        + 'group' + ',' + 'model1' + ',' + 'model2' + ',' + 'model3' + ',' + 'block' + '\n')
     for i in range(len(result['average_r'])):
         exp_data.write(str(result['id'][i]) + ',' + result['name'] + ',' + str(result['age']) + ',' + result['sex']
                        + ',' + str(round(result['average_r'][i], 2)) + ',' + str(result['cover'][i]) + ',' +
                        str(result['estimate'][i]) + ',' + result['group'][i] + ',' + str(result['m_1'][i]) + "," +
-                       str(result['m_2'][i]) + ',' + str(result['block'][i]) + '\n')
+                       str(result['m_2'][i]) + ',' + str(result['m_3'][i]) + "," + str(result['block'][i])
+                       + "," + str(adjust_size[i]) + '\n')
 
 visual.TextStim(win, text="实验结束！", height=64).draw()
 win.flip()
